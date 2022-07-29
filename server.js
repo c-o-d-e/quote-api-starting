2d237969038a49305ccdef172496f57b68a20871
c-o-d-e <swapyuk@gmail.com>
2022-07-29 20:33:51 +0100

    random quote get method

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
 
+app.get("/api/quotes/random", (req, res, next) => {
+    const quoteExpression = getRandomElement(quotes);
+    res.send({ quote: quoteExpression });
+});
+
 app.listen(PORT, () => {
     console.log("The server has started");
 });
